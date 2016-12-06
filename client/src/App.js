925305ed04e974d8a3b55bb21f7cb1b1057c36dd
johntrecker <jttrecker@gmail.com>
2016-12-05 18:15:48 -0800

    collect button assets

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,49 @@
 import React from 'react';
-import SelectedFoods from './SelectedFoods';
-import FoodSearch from './FoodSearch';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import './assets/index.css';
+let keys = require('./config/api_keys.json')
 
-const App = React.createClass({
-  getInitialState: function () {
-    return {
-      selectedFoods: [],
-    };
-  },
-  render: function () {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      location: [-122.413692, 37.775712],
+      venues: 'restaurants',
+    }
+  }
+  update(e){
+    this.setState({venues: e.target.value})
+  }
+  componentWillMount(){
+  }
+  render(){
     return (
-      <div className='App'>
-        <div className='ui text container'>
-          <SelectedFoods
-            foods={this.state.selectedFoods}
-            onFoodClick={
-              (idx) => (
-                this.setState({
-                  selectedFoods: [
-                    ...this.state.selectedFoods.slice(0, idx),
-                    ...this.state.selectedFoods.slice(
-                      idx + 1, this.state.selectedFoods.length
-                    ),
-                  ],
-                })
-              )
-            }
-          />
-          <FoodSearch
-            onFoodClick={
-              (food) => (
-                this.setState({
-                  selectedFoods: this.state.selectedFoods.concat(food),
-                })
-              )
-            }
-          />
-        </div>
+      <div id="map">
+        <Nav update={this.update.bind(this)} />
       </div>
-    );
-  },
-});
 
-export default App;
+    )
+  }
+  componentDidMount(){
+    mapboxgl.accessToken = keys.mapboxgl_access_token;
+    let mapbox = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: this.state.location,
+        zoom: 12
+    });
+  }
+}
+
+const Nav = (props) => (
+  <div className="ui three buttons">
+    <button className="ui button" onClick={props.update}>Food/Drink</button>
+    <div className="or"></div>
+    <button className="ui button" onClick={props.update}>Parks</button>
+    <div className="or"></div>
+    <button className="ui button" onClick={props.update}>Events</button>
+  </div>
+)
+
+
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './assets/index.css';
 import './semantic-ui/semantic.min.css';
 
 ReactDOM.render(
