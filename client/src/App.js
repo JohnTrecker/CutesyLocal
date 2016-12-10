879de44d816a3499c188d6647bdd6d4f7d295c77
johntrecker <jttrecker@gmail.com>
2016-12-09 18:59:41 -0800

    restyle buttons with flexbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
 // eslint-disable-next-line
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './assets/index.css';
 import data from './yelp.json';
 let keys = require('./config/api_keys.json');
-let coffee = require('./assets/icon_coffee.png');
-let park = require('./assets/icon_park.png');
-let event = require('./assets/icon_event.png');
+let btn_coffee = require('./assets/icon_btn_coffee.png');
+let btn_park = require('./assets/icon_btn_park.png');
+let btn_event = require('./assets/icon_btn_event.png');
+let mkr_coffee = require('./assets/icon_mkr_coffee.png');
+let mkr_park = require('./assets/icon_mkr_park.png');
+let mkr_event = require('./assets/icon_mkr_event.png');
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       location: [-122.413692, 37.775712],
-      venues: null,
+      visibleVenues: [],
     }
   }
   update(e){
-    this.setState({venues: e.target.value})
+    console.log(e.taget);
+    let type = e.target.className;
+    let toggledButtons = this.state.visibleVenues;
+
+    if (toggledButtons.includes(type)) {
+      let i = toggledButtons.indexOf(type);
+      toggledButtons.slice(i, 1);
+    } else {
+      toggledButtons.push(type);
+    }
+    this.setState({visibleVenues: toggledButtons})
   }
   componentWillMount(){
     mapboxgl.accessToken = keys.mapboxgl_access_token;
@@ -34,8 +46,8 @@ class App extends React.Component {
       data.businesses.forEach(function(marker) {
           // create an img element for the marker
           let el = document.createElement('div');
-          let venue = 'restaurant' //marker.categories[0].alias
-          let img_url = venue === 'dog_parks' ? park : (venue === 'event' ? event : coffee);
+          let venue = 'mkr-restaurant' //marker.categories[0].alias
+          let img_url = venue === 'dog_parks' ? mkr_park : (venue === 'event' ? mkr_event : mkr_coffee);
           let coordinates = [].concat(marker.coordinates.longitude, marker.coordinates.latitude);
 
           el.className = 'marker ' + venue;
@@ -49,8 +61,16 @@ class App extends React.Component {
   }
   render(){
     return (
-      <div>
-        <Nav update={this.update.bind(this)} />
+      <div className="nav">
+        <Button update={this.update.bind(this)} class="restaurant">
+          <div className="btn-contents"><Image icon={btn_coffee}/> Food/Drink</div>
+        </Button>
+        <Button update={this.update.bind(this)} class="park">
+          <div className="btn-contents"><Image icon={btn_park}/> Parks</div>
+        </Button>
+        <Button update={this.update.bind(this)} class="event">
+          <div className="btn-contents"><Image icon={btn_event}/> Events</div>
+        </Button>
       </div>
     )
   }
@@ -58,32 +78,9 @@ class App extends React.Component {
   }
 }
 
-class Nav extends React.Component {
-  constructor(){
-    super();
-  }
-  render(){
-    return (
-      <div className="ui three buttons">
-        <Button onClick={this.update} classes="ui toggle button blue"><Image icon={coffee}/> Food/Drink</Button>
-        <div className="or"></div>
-        <Button classes="ui toggle button green"><Image icon={park}/> Parks</Button>
-        <div className="or"></div>
-        <Button classes="ui toggle button red"><Image icon={event}/> Events</Button>
-      </div>
-    )
-  }
-}
+let Button = (props) => <button className={ props.class } onClick={ props.update } >{ props.children }</button>
 
-const Button = (props) =>
-  <button className={props.classes}>
-    { props.children }
-  </button>
+const Image = (props) => <img src={ props.icon } role="presentation" />
 
-class Image extends React.Component {
-  render(){
-    return <img className="ui avatar image" src={this.props.icon} role="presentation" />
-  }
-}
 
 export default App
