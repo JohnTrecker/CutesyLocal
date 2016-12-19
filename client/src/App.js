89af574f24fab52b944d8ed204eb743eeb73ffef
johntrecker <jttrecker@gmail.com>
2016-12-19 00:42:22 -0800

    relegate button and marker styling to css

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
-// eslint-disable-next-line
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'; // eslint-disable-line no-console
 import React from 'react';
 import './assets/index.css';
-import data from './yelp.json';
+let restaurantData = require('./data/yelp.json');
+let parkData = require('./data/parks.json');
+let eventData = require('./data/events.json');
 let $ = require('jquery');
 let keys = require('./config/api_keys.json');
-let btn_coffee = require('./assets/icon_btn_coffee.png');
-let btn_park = require('./assets/icon_btn_park.png');
-let btn_event = require('./assets/icon_btn_event.png');
-let mkr_coffee = require('./assets/icon_mkr_coffee.png');
-let mkr_park = require('./assets/icon_mkr_park.png');
-let mkr_event = require('./assets/icon_mkr_event.png');
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       location: [-122.413692, 37.775712],
+      restaurantData: restaurantData,
+      parkData: parkData,
+      eventData: eventData,
       visibleVenues: [],
     }
   }
-  update(e){
+  updatevisibleVenues(e){
     let classes = ['restaurant', 'park', 'event'];
     let venue;
     let toggledButtons = this.state.visibleVenues;
@@ -53,8 +51,44 @@ class App extends React.Component {
     }
     this.setState({visibleVenues: toggledButtons});
   }
-
+  updateData(data){
+    let venueData = [data[0].properties.venue, 'Data'].join();
+    this.setState( '{' + venueData + ': ' + data + '}' );
+  }
   componentWillMount(){
+    // let that = this;
+
+    // // TODO: reset state with current yelp data
+    // fetch( 'http://localhost:3000/api/yelp' )
+    //   .then( (res) => res.json() )
+    //   .then( function(results){
+    //     that.setState( {restaurantData: results} )
+    //     // Bug: Resets `that` but never affects `this`
+    //     //      How does egghead.io make it work?
+    //   })
+    //   .catch( function(e){
+    //     debugger;
+    //     console.log(e);
+    //   })
+
+  }
+  render(){
+    // TODO: refactor uneccessary icon names
+    return (
+      <div className="nav">
+        <Button updatevisibleVenues={this.updatevisibleVenues.bind(this)} class="restaurant">
+          <div className="btn-contents"><Image class="restaurant"/> Food/Drink</div>
+        </Button>
+        <Button updatevisibleVenues={this.updatevisibleVenues.bind(this)} class="park">
+          <div className="btn-contents"><Image class="park"/> Parks</div>
+        </Button>
+        <Button updatevisibleVenues={this.updatevisibleVenues.bind(this)} class="event">
+          <div className="btn-contents"><Image class="event"/> Events</div>
+        </Button>
+      </div>
+    )
+  }
+  componentDidMount(){
     mapboxgl.accessToken = keys.mapboxgl_access_token;
     let map = new mapboxgl.Map({
         container: 'map',
@@ -63,46 +97,35 @@ class App extends React.Component {
         zoom: 12
     });
 
+    let state = [this.state.eventData, this.state.parkData, this.state.restaurantData];
     map.on('load', function(){
+
+      //TODO: clean up previous markers
+      //      delete zombie code
       let markers = {};
-      data.businesses.forEach(function(marker) {
-          // create an img element for the marker
+
+      state.forEach(function(data){
+        data.forEach(function(marker) {
           let el = document.createElement('div');
-          let venue = 'mkr-restaurant' //marker.categories[0].alias
-          let img_url = venue === 'dog_parks' ? mkr_park : (venue === 'event' ? mkr_event : mkr_coffee);
-          let coordinates = [].concat(marker.coordinates.longitude, marker.coordinates.latitude);
+          // let venue = marker.properties.venue;
+          // let img_url = venue === 'park' ? mkr_park : (venue === 'event' ? mkr_event : mkr_coffee);
+          let coordinates = marker.geometry.coordinates;
 
-          el.className = venue;
-          el.style.backgroundImage = "url(" + img_url + ")";
+          el.className = 'mkr-' + marker.properties.venue;
+          // el.style.backgroundImage = "url(" + img_url + ")";
 
-          markers[marker.name] = new mapboxgl.Marker(el)
+          markers[marker.properties.name] = new mapboxgl.Marker(el)
               .setLngLat(coordinates)
               .addTo(map);
+        });
       });
-    })
-  }
-  render(){
-    return (
-      <div className="nav">
-        <Button update={this.update.bind(this)} class="restaurant">
-          <div className="btn-contents"><Image icon={btn_coffee}/> Food/Drink</div>
-        </Button>
-        <Button update={this.update.bind(this)} class="park">
-          <div className="btn-contents"><Image icon={btn_park}/> Parks</div>
-        </Button>
-        <Button update={this.update.bind(this)} class="event">
-          <div className="btn-contents"><Image icon={btn_event}/> Events</div>
-        </Button>
-      </div>
-    )
-  }
-  componentDidMount(){
+    });
   }
 }
 
-let Button = (props) => <button className={ props.class } onClick={ props.update } >{ props.children }</button>
+let Button = (props) => <button className={ props.class } onClick={ props.updatevisibleVenues } >{ props.children }</button>
 
-const Image = (props) => <img src={ props.icon } role="presentation" />
+const Image = (props) => <img className={ props.class } role="presentation" />
 
 
 export default App
