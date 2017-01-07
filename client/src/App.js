1e70b70950384cd5d8274d3f3457e66e64e0c2b7
johntrecker <jttrecker@gmail.com>
2017-01-07 06:38:22 -0800

    incude dates in event popups; wrap all popup description text

--- client/src/App.js
+++ client/src/App.js
@@ -222,6 +222,7 @@ const Popup = function(props){
   const rating = props.marker.rating * 20;
   const ratingClass = rating >= 80 ? 'great' : (rating < 70 ? 'notsogood' : 'good');
   const location = JSON.parse(props.marker.location);
+  const showDates = Boolean(props.marker.dates);
   return (
     <div className="popup-contents">
       <div className="image">
@@ -229,7 +230,8 @@ const Popup = function(props){
       </div>
       <div className="description">
         <p className="title">{ props.marker.name }</p>
-        <p className="address">{ location.address1 }</p>
+        <p className="address">{ location.address1 } </p>
+        { showDates && <Dates timeAndDate={props.marker.dates} /> }
         <div className="rating">
           <img className={ ratingClass } alt="http://emojipedia-us.s3.amazonaws.com/cache/6b/16/6b164a624288271a884ab2a22f9bb693.png" />
           <p className="percentage">&ensp; { rating } </p>
@@ -242,5 +244,9 @@ const Popup = function(props){
     </div>
   )
 };
+
+const Dates = function(props){
+  return <p className="dates"> {props.timeAndDate} </p>
+}
 
 export default App
