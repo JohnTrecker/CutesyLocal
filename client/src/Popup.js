381c17310a88c71eb34967c853e0768bc8af8e5a
johntrecker <jttrecker@gmail.com>
2017-01-20 13:31:44 -0600

    refactor Popup component hidden content

--- client/src/Popup.js
+++ client/src/Popup.js
@@ -6,11 +6,10 @@ class Popup extends React.Component {
   render(){
     const rating = this.props.marker.rating * 20;
     const ratingClass = rating >= 80 ? 'great' : (rating < 70 ? 'notsogood' : 'good');
-    const address = this.props.marker.address;
-    const showDates = Boolean(this.props.marker.dates !== "null");
+    const showDates = this.props.marker.dates !== "null";
     const showReview = this.props.marker.reviews !== "[]";
-    const review = showReview === true ? JSON.parse(this.props.marker.reviews)[0].review : "Been here before? Leave a review.";
-    const reviewer = showReview === true ? JSON.parse(this.props.marker.reviews)[0].reviewer : "";
+    const review = showReview === true ? JSON.parse(this.props.marker.reviews)[0].review : null;
+    const reviewer = showReview === true ? JSON.parse(this.props.marker.reviews)[0].reviewer.toLowerCase() : null;
     return (
       <div className="popup-contents ui slide left instant reveal">
         <div className="visible content">
@@ -19,7 +18,7 @@ class Popup extends React.Component {
           </div>
           <div className="description">
             <p className="title">{ this.props.marker.name }</p>
-            <p className="address">{ address }</p>
+            <p className="address">{ this.props.marker.address }</p>
             { showDates && <Dates timeAndDate={ this.props.marker.dates } /> }
             <div className="rating">
               <img className={ ratingClass } alt="http://emojipedia-us.s3.amazonaws.com/cache/6b/16/6b164a624288271a884ab2a22f9bb693.png" />
@@ -28,15 +27,8 @@ class Popup extends React.Component {
             </div>
           </div>
         </div>
-        <div className="hidden content">
-          <div className="review-image">
-            <img id="reviewer" className={reviewer.toLowerCase()} role="presentation"/>
-          </div>
-          <div className="review-info">
-            <p className="title">{ reviewer }</p>
-            <p className="address">{ review }</p>
-          </div>
-        </div>
+          { showReview && <Reviews review={ review } reviewer={ reviewer }/> }
+          { !showReview && <NoReviews name={ this.props.marker.name }/> }
       </div>
     )
   }
@@ -44,5 +36,20 @@ class Popup extends React.Component {
 
 const Dates = (props) =>
   <p className="dates"> {props.timeAndDate} </p>
+
+const Reviews = (props) =>
+  <div className="hidden content" >
+    <div className="review-image">
+      <img id="reviewer" className={ props.reviewer} role="presentation"/>
+    </div>
+    <div className="review-info">
+      <p className="title">{ props.reviewer }</p>
+      <p className="address">{ props.review }</p>
+    </div>
+  </div>
+
+const NoReviews = (props) =>
+  <p className="hidden content address">Been to { props.name } before?&ensp;<a href="/login/facebook">Leave a review</a>.</p>
+
 
 export default Popup;
