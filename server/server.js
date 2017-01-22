295848bc1f5f6b5c9f27ee0f04dc538bfa753d41
johntrecker <jttrecker@gmail.com>
2017-01-21 21:39:13 -0600

    hide Facebook id and secret

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const app = express();
-const apiRouter = require('./resources/api/apiRouter');
-const bodyParser = require('body-parser');
-const helpers = require('./resources/lib/helpers');
-let restaurants = require('../client/src/data/restaurant.json');
-let parks = require('../client/src/data/event.json');
-let events = require('../client/src/data/park.json');
+const express = require('express')
+    , bodyParser = require('body-parser')
+    , passport = require('passport')
+    , Strategy = require('passport-facebook').Strategy
+    , apiRouter = require('./resources/api/apiRouter')
+    , helpers = require('./resources/lib/helpers')
+    , id = require('./config/config').facebook_client_id
+    , secret = require('./config/config').facebook_client_secret;
 
+let app = express();
 app.set('port', (process.env.PORT || 3001));
-
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
@@ -22,4 +22,5 @@ app.use('/api', apiRouter);
 
 app.listen(app.get('port'), () => {
   console.log(`Server listening at http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+  passport.authenticate('facebook')
 });
