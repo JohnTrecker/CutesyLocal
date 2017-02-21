f156a3d3e581e8f359057bb1087d498b21472375
johntrecker <jttrecker@gmail.com>
2017-02-21 14:53:30 -0700

    removes heavey semantic ui folder with lighter css dependency

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './assets/index.css';
-import './semantic-ui/semantic.min.css';
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <App />,
