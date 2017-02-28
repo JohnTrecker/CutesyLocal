54df0ff1c7c14b58f6d680e4ae39e139781026c7
johntrecker <jttrecker@gmail.com>
2017-02-28 12:41:31 -0800

    add script for zeit deploy secret env variables

--- client/src/Login.js
+++ client/src/Login.js
@@ -5,6 +5,7 @@ import { Button, Image, Modal } from 'semantic-ui-react';
 class Login extends React.Component {
   render(){
     const { open, toggleModal, setUser} = this.props;
+    const appId = process.env.FACEBOOK_CLIENT_ID;
     const responseFacebook = (response) => setUser(response);
     return (
         <Modal size="small" dimmer='blurring' open={ open } onClose={ toggleModal }>
@@ -15,7 +16,7 @@ class Login extends React.Component {
           <Modal.Actions>
             <Button color='black' onClick={ toggleModal }>No thanks</Button>
             <FacebookLogin
-              appId='1247036205362895'
+              appId={appId}
               autoLoad={false}
               fields="name,email,picture"
               callback={responseFacebook}
