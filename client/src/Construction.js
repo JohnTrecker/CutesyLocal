10c73f857fa6ca025ea6f5cbe71487ad6c6f16d0
johntrecker <jttrecker@gmail.com>
2017-07-06 19:08:14 -0500

    includes scheduled maintenance details to Construction component

--- client/src/Construction.js
+++ client/src/Construction.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
+function formatDate() {
+  let monthNames = [
+    "Jan", "Feb", "Mar",
+    "Apr", "May", "Jun", "Jul",
+    "Aug", "Sept", "Oct",
+    "Nov", "Dec"
+  ];
+  let date = new Date();
+  let monthIndex = date.getMonth();
+  let day = date.getDate();
+
+  return monthNames[monthIndex] + ' ' + day;
+}
+
 const Construction = () => (
     <div className="home-temp">
       <div className="banner-temp"></div>
       <div className="gradient-temp">
         <section className="container-temp">
-          <h2>Mobile app under scheduled maintainance July 6, 5pm-5am PST</h2>
+          <h2>Mobile app under scheduled maintainance {formatDate(new Date())}</h2>
           <h4>Visit us on your laptop at cutesylocal.com</h4>
         </section>
         <div className="logo-temp"></div>
